eac3b12b27fbf9cd068c6d855d4eaeddcddf5826
pavankrishna08 <pavan.shobith@gmail.com>
2023-12-31 18:54:21 +0530

    git commit

--- test_fix_sample.py
+++ test_fix_sample.py
@@ -7,3 +7,20 @@ class Fruit:
 
     def cube(self):
         self.cubed = True
+
+class FruitSalad:
+    def __init__(selfself,*fruit_bowl):
+        self.fruit = fruit_bowl
+        self._cube_fruit()
+
+    def _cube_fruit(self):
+        for fruit in self.fruit:
+            fruit.cube()
+
+@pytest.fixture
+def fruit_bowl():
+    return [Fruit('apple'),Fruit('banana')]
+
+def test_fruit_salad(fruit_bowl):
+    fruit_salad = FruitSalad(*fruit_bowl)
+    assert all(fruit.cubed for fruit in fruit_salad.fruit)
